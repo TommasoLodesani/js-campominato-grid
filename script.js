6fd140a95d4638098ed63d4eb0b955266f86c2f2
Tommaso <tommaso.lodesani@gmail.com>
2022-05-13 17:28:20 +0200

    correzione variabile sceltaUtente

--- script.js
+++ script.js
@@ -1,82 +1,91 @@
+
+
 const myGrid = document.getElementById("grid");
 
-const sceltaUtente = document.getElementById("inputText").value;
+const myButton = document.getElementById("play");
 
-const myButton = document.getElementById("play")
+const myButtonReset = document.getElementById("reset");
 
 // let sceltaUtente = prompt("Scegli livello");
 
+
+
 myButton.addEventListener("click",
 
-    function(){
+    function () {
 
-        if(sceltaUtente ==="facile"){
+        let sceltaUtente = document.getElementById("inputText").value;
+        console.log(sceltaUtente);
+
+
+
+        if (sceltaUtente === "facile") {
 
             const createMyElement = () => {
                 const node = document.createElement("div");
                 node.className = "square_easy";
                 return node;
             }
-        
-            for ( let i = 0; i < 100; i++){
+
+            for (let i = 0; i < 100; i++) {
                 const divEasy = createMyElement();
-        
+
                 divEasy.addEventListener("click",
-        
-                    function(){
+
+                    function () {
                         this.classList.add("clicked-green");
                     }
                 )
-        
+
                 myGrid.append(divEasy);
             }
             console.log("hai scelto facile");
-        
-        
-        } else if (sceltaUtente === "medio"){
-        
+
+
+        } else if (sceltaUtente === "medio") {
+
             function createMyElement() {
                 const node = document.createElement("div");
                 node.className = "square_medium";
                 return node;
             }
-        
-            for ( let i = 0; i < 81; i++){
+
+            for (let i = 0; i < 81; i++) {
                 const divMedium = createMyElement();
-        
+
                 divMedium.addEventListener("click",
-        
-                    function(){
+
+                    function () {
                         this.classList.add("clicked-green");
                     }
                 )
-                
+
                 myGrid.append(divMedium);
             }
             console.log("hai scelto medio");
-        
-        }else if(sceltaUtente === "difficile"){
-        
+
+        } else if (sceltaUtente === "difficile") {
+
             const createMyElement = () => {
                 const node = document.createElement("div");
                 node.className = "square_hard";
                 return node;
             }
-        
-            for ( let i = 0; i < 49; i++){
+
+            for (let i = 0; i < 49; i++) {
                 const divHard = createMyElement();
-        
+
                 divHard.addEventListener("click",
-        
-                    function(){
+
+                    function () {
                         this.classList.add("clicked-green");
                     }
-        
+
                 )
-        
+
                 myGrid.append(divHard);
             }
-           
+
             console.log("hai scelto difficile");
         }
 
@@ -86,3 +95,10 @@ myButton.addEventListener("click",
 
 )
 
+myButtonReset.addEventListener("click",
+
+    function () {
+        document.getElementById("inputText").value = "";
+    }
+
+)
