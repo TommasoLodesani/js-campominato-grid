1f34ddd46a101ed6a1413e06991236c291f2efc7
Tommaso <tommaso.lodesani@gmail.com>
2022-05-13 16:08:37 +0200

    output html button

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
 const myGrid = document.getElementById("grid");
 
-let sceltaUtente = prompt("Scegli livello");
+const sceltaUtente = document.getElementById("inputText").value;
 
+const myButton = document.getElementById("play")
 
-if(sceltaUtente ==="facile"){
+// let sceltaUtente = prompt("Scegli livello");
 
-    const createMyElement = () => {
-        const node = document.createElement("div");
-        node.className = "square_easy";
-        return node;
-    }
+myButton.addEventListener("click",
 
-    for ( let i = 0; i < 100; i++){
-        const divEasy = createMyElement();
+    function(){
 
-        divEasy.addEventListener("click",
+        if(sceltaUtente ==="facile"){
 
-            function(){
-                this.classList.add("clicked-green");
+            const createMyElement = () => {
+                const node = document.createElement("div");
+                node.className = "square_easy";
+                return node;
             }
-        )
-
-        myGrid.append(divEasy);
-    }
-    console.log("hai scelto facile");
-
-
-} else if (sceltaUtente === "medio"){
-
-    function createMyElement() {
-        const node = document.createElement("div");
-        node.className = "square_medium";
-        return node;
-    }
-
-    for ( let i = 0; i < 81; i++){
-        const divMedium = createMyElement();
-
-        divMedium.addEventListener("click",
-
-            function(){
-                this.classList.add("clicked-green");
-            }
-        )
         
-        myGrid.append(divMedium);
-    }
-    console.log("hai scelto medio");
-
-}else if(sceltaUtente === "difficile"){
-
-    const createMyElement = () => {
-        const node = document.createElement("div");
-        node.className = "square_hard";
-        return node;
-    }
-
-    for ( let i = 0; i < 49; i++){
-        const divHard = createMyElement();
-
-        divHard.addEventListener("click",
-
-            function(){
-                this.classList.add("clicked-green");
+            for ( let i = 0; i < 100; i++){
+                const divEasy = createMyElement();
+        
+                divEasy.addEventListener("click",
+        
+                    function(){
+                        this.classList.add("clicked-green");
+                    }
+                )
+        
+                myGrid.append(divEasy);
             }
+            console.log("hai scelto facile");
+        
+        
+        } else if (sceltaUtente === "medio"){
+        
+            function createMyElement() {
+                const node = document.createElement("div");
+                node.className = "square_medium";
+                return node;
+            }
+        
+            for ( let i = 0; i < 81; i++){
+                const divMedium = createMyElement();
+        
+                divMedium.addEventListener("click",
+        
+                    function(){
+                        this.classList.add("clicked-green");
+                    }
+                )
+                
+                myGrid.append(divMedium);
+            }
+            console.log("hai scelto medio");
+        
+        }else if(sceltaUtente === "difficile"){
+        
+            const createMyElement = () => {
+                const node = document.createElement("div");
+                node.className = "square_hard";
+                return node;
+            }
+        
+            for ( let i = 0; i < 49; i++){
+                const divHard = createMyElement();
+        
+                divHard.addEventListener("click",
+        
+                    function(){
+                        this.classList.add("clicked-green");
+                    }
+        
+                )
+        
+                myGrid.append(divHard);
+            }
+           
+            console.log("hai scelto difficile");
+        }
 
-        )
 
-        myGrid.append(divHard);
+
     }
-   
-    console.log("hai scelto difficile");
-}
+
+)
+
